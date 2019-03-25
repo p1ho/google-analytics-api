4116150fc0a4aaea3861322f5cbd9b1cbcb0951c
hopokuan <hopokuan@umich.edu>
2019-03-25 18:44:16 -0400

    Added Client.php with tests

---
+++ src/Client.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace P1ho\GoogleAnalyticsAPI;
+
+class Client
+{
+    private $fetcher;
+
+    public function __construct(string $pathToCredentials)
+    {
+        $this->fetcher = new Report\Fetcher($pathToCredentials);
+    }
+
+    public function getData(string $viewId, string $startDate, string $endDate, array $dimensions, array $metrics, string $filtersExp = '')
+    {
+        $package = new Request\Package($viewId, $startDate, $endDate, $dimensions, $metrics, $filtersExp);
+        $fetchedData = $this->fetcher->getData($package);
+        return Report\Assembler::run($fetchedData);
+    }
+}
