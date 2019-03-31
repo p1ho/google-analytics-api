9603f62e172aa563a9eaf98ae44887117fe2a857
hopokuan <hopokuan@umich.edu>
2019-03-30 21:36:20 -0400

    Removed tests that require secrets.json

--- tests/Report/FetcherTest.php
+++ tests/Report/FetcherTest.php
@@ -2,8 +2,6 @@
 
 use PHPUnit\Framework\TestCase;
 use P1ho\GoogleAnalyticsAPI\Report\Fetcher;
-use P1ho\GoogleAnalyticsAPI\Report\FetchedData;
-use P1ho\GoogleAnalyticsAPI\Request\Package;
 
 final class FetcherTest extends TestCase
 {
@@ -19,47 +17,18 @@ final class FetcherTest extends TestCase
         }
         $this->assertTrue($hasError);
 
-        /*
-        To run this test, there needs to be a secrets.json at package root that
-        contains the following:
-        {
-          "credentials": "path-to-your-google-credential-file",
-          "viewId": "a view id that your account has access to"
-        }
-         */
-
-        $secretsPath = __DIR__ . '/../../secrets.json';
-        if (!file_exists($secretsPath)) {
-            throw new Exception("You haven't set up secrets.json at package root, please see README.md development section.");
-        }
-
-        $secretsRaw = file_get_contents($secretsPath);
-        $secrets = json_decode($secretsRaw);
-        $fetcher = new Fetcher($secrets->credentials);
-
-        // sending real requests, so use pretested dimensions/metrics
-        $compatibleDimensionsMetrics = json_decode(
-            file_get_contents(__DIR__ . '/LongestCompatibleDimensionsMetrics.json')
-        );
-
-        $viewId = $secrets->viewId;
-        $startDate = 'yesterday';
-        $endDate = 'today';
-        $dimensions = array_slice($compatibleDimensionsMetrics->dimensions, 0, 7);
-        $metrics = array_slice($compatibleDimensionsMetrics->metrics, 0, 50);
-        $package = new Package($viewId, $startDate, $endDate, $dimensions, $metrics);
-
-        // no error should be thrown when get Data is called
+        // if credentials file exist, it should instantiate without error
         $hasError = false;
         try {
-            $report = $fetcher->getData($package);
+            $fetcher = new Fetcher(__DIR__ . '/TestCredentials.json');
         } catch (Exception $e) {
             $hasError = true;
         }
         $this->assertFalse($hasError);
-        $this->assertEquals(get_class($report), 'P1ho\GoogleAnalyticsAPI\Report\FetchedData');
 
-        // can't reliably test other things because this will be vastly different
-        // across sites.
+        /*
+        not testing the batchGet call because it will be inconsistent across devices
+        and sites, and it's not really testing the System Under Testing.
+         */
     }
 }
